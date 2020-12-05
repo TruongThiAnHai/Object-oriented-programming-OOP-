afb17d8ad3c3a95691ab96530fda61f11d62ff82
Truong Thi An Hai <55710763+TruongThiAnHai@users.noreply.github.com>
2020-12-05 13:41:17 +0300

    Add files via upload

---
+++ Project5/Project5/Source.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+//#include <windows.h>
+#include <map>
+
+using namespace std;
+
+
+int main()
+{
+	
+		//вставляемая строка
+		string insertString;
+		cout << "Enter the insertion string: ";
+		getline(cin, insertString);
+		string decTo2;
+		cout << insertString.size() << endl;
+		//преобразование ascii кода вставляемой строки в двоичный вид
+		for (size_t ind = 0; ind <insertString.size(); ++ind) {
+			int ascii = (int)insertString[ind];
+			cout << ascii << endl;
+			string binary;
+			do {
+				binary = to_string(ascii % 2).c_str() + binary;
+				ascii = (int)ascii / 2;
+			} while (ascii > 0);
+			decTo2 += binary;
+		}
+		cout << decTo2<<endl;
+	system("pause");
+	return 0;
+}
